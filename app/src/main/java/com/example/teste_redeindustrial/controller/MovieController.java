50b7647fc93caa68b7ce302b3fbf570834d0968d
Helder Rodrigues Mendes <helderrodrigues37@gmail.com>
2020-08-20 23:08:13 -0300

    "alterando classe de requisição"

--- app/src/main/java/com/example/teste_redeindustrial/controller/MovieController.java
+++ app/src/main/java/com/example/teste_redeindustrial/controller/MovieController.java
@@ -1,13 +1,45 @@
 package com.example.teste_redeindustrial.controller;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.teste_redeindustrial.model.Movie;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
 public class MovieController {
-    public List<Movie> buscarFilme(String titulo, String id){
+    private RequestQueue requestQueue;
+    Movie movie = new Movie();
+    public Movie buscarFilme(String titulo, String id){
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, montarURL(titulo, id), null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray jsonArray) {
+                try {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject MOVIE = jsonArray.getJSONObject(i);
+                        movie = new Gson().fromJson(MOVIE.toString(), Movie.class);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+            }
+        });
+        requestQueue.add(request);
+        System.out.println("TITLE: " + movie.getTitle());
 
-        return null;
+        return movie;
     }
 
     private String montarURL(String titulo, String id){
--- app/src/main/java/com/example/teste_redeindustrial/view/HomeActivity.java
+++ app/src/main/java/com/example/teste_redeindustrial/view/HomeActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.teste_redeindustrial.R;
+import com.example.teste_redeindustrial.controller.MovieController;
+import com.example.teste_redeindustrial.model.Movie;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,5 +14,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        editTitulo = findViewById(R.id.edit_titulo);
+        editID = findViewById(R.id.edit_id);
+        btn_pesquisar = findViewById(R.id.btnpesquisar);
+
+        btn_pesquisar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!editTitulo.getText().toString().equals("") && !editID.getText().toString().equals("")){
+                    Toast.makeText(HomeActivity.this, "Faça apenas 1 tipo de pesquisa", Toast.LENGTH_SHORT).show();
+                }else{
+                    titulo = editTitulo.getText().toString();
+                    id = editID.getText().toString();
+                    Movie movie = new Movie();
+                    movie = movieController.buscarFilme(titulo, id);
+                }
+            }
+        });
     }
 }
